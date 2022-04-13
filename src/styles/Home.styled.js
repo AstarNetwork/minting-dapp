af53f5b6f0fcff1a936eaf5c0e6133c10d0203b0
impelcrypto <impelcrypto@gmail.com>
2022-04-14 00:29:57 +0800

    styling: reverse the widget and text description for mobile devices

--- src/styles/Home.styled.js
+++ src/styles/Home.styled.js
@@ -13,13 +13,15 @@ export const HomeContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  flex-direction: column;
+  flex-direction: column-reverse;
   row-gap: 16px;
-  margin-bottom: 24px;
+  margin-top: 12px;
+  margin-bottom: 12px;
   @media (min-width: 768px) {
     column-gap: 12px;
     flex-direction: row;
     margin-bottom: 0px;
+    margin-top: 0px;
   }
   @media (min-width: 1024px) {
     column-gap: 38px;
