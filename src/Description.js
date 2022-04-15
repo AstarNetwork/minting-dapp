f49e79574ad277946f088e05734ad9c553db26f3
impelcrypto <impelcrypto@gmail.com>
2022-04-15 21:13:51 +0800

    fix: words

--- src/Description.js
+++ src/Description.js
@@ -19,22 +19,20 @@ const Description = () => {
       <div>
         <RowDescription>
           <TextDescription>
-<<<<<<< HEAD
-            To claim one free NFT you need to be an active staker in
-            <TextDescriptionPrimary>
-              {' '}
-              dApp Staking on Astar{' '}
-            </TextDescriptionPrimary>
-            and you need to be registered as
-            <TextDescriptionPrimary> AstarPass </TextDescriptionPrimary>
-            holder. Otherwise, you can still mint 1 NFT but you need to pay.
-=======
             To claim one free NFT you need to be an active staker in{' '}
-            <LinkPrimary target='_blank' href='https://portal.astar.network/#/dapp-staking/discover'>dApp Staking on Astar</LinkPrimary>{' '}
+            <LinkPrimary
+              target="_blank"
+              href="https://portal.astar.network/#/dapp-staking/discover">
+              dApp Staking on Astar
+            </LinkPrimary>{' '}
             and you need to be registered as
-            <LinkPrimary href='https://astarpass.astar.network/#/register' target='_blank'> AstarPass </LinkPrimary>
-            holder. Otherwise you can still mint 1 NFT but you need to pay.
->>>>>>> 9048700961dfe2f833bf7c92f25cf1cd42abb741
+            <LinkPrimary
+              href="https://astarpass.astar.network/#/register"
+              target="_blank">
+              {' '}
+              AstarPass{' '}
+            </LinkPrimary>
+            holder. Otherwise, you can still mint 1 NFT but you need to pay.
             Please make sure you are connected to Astar Network and the correct
             address. Please note: Once you make the purchase, you cannot undo
             this action.
